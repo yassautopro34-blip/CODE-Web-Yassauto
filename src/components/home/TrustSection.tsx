8cf24b0175c89c3f4c4578275d949ca6a157712b
YassAuto <Yassauto.pro34@gmail.com>
2026-04-06 01:16:29 +0200

    fix: tous les liens Google Maps pointent vers https://maps.app.goo.gl/79vPS7FCQyuf3Pge6?g_st=ic

--- src/components/home/TrustSection.tsx
+++ src/components/home/TrustSection.tsx
@@ -87,7 +87,7 @@ export const TrustSection: React.FC = () => {
             </div>
 
             <a
-              href="https://www.google.com/maps/place/YASSAUTO+MKLF/@43.4920827,3.7051598,17z/data=!3m1!4b1!4m6!3m5!1s0x12b14beb886c2d05:0x7674229ccefd9e45!8m2!3d43.4920827!4d3.7051598!16s%2Fg%2F11n58n0vkz?entry=ttu&g_ep=EgoyMDI2MDQwMS4wIKXMDSoASAFQAw%3D%3D"
+              href="https://maps.app.goo.gl/79vPS7FCQyuf3Pge6?g_st=ic"
               target="_blank"
               rel="noopener noreferrer"
               className="inline-flex items-center gap-2 text-brand-red font-bold text-sm hover:gap-3 transition-all"
